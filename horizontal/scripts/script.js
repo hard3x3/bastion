3040f1adf89c39264276f4d960fe43c738ab9281
hard3x3.eth <140728609+hard3x3@users.noreply.github.com>
2023-11-02 20:07:50 -0400

    Update script.js

--- horizontal/scripts/script.js
+++ horizontal/scripts/script.js
@@ -1,1 +1,8 @@
-
+// Function to parse query parameters from the URL
+function getQueryParameters() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    return urlParams;
+}
+    const queryParams = getQueryParameters();
+    const queryParamValue = queryParams.get("q");
